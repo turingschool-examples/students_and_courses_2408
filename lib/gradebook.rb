38e7a7d5f20bf530ed007b45622a9226eaeedd8f
Michael OBrien <obrien.michael.t@gmail.com>
2024-09-04 16:57:46 -0400

    Refactored Code to clean it up

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -1,6 +1,6 @@
 class Gradebook
-    attr_reader :instructor
-                :course
+    attr_reader :instructor,
+                :courses,
                 :list_all_students
 
     def initialize(instructor)
@@ -9,19 +9,17 @@ class Gradebook
         @list_all_students = {}
     end
 
-    def instructor
-        @instructor
-    end
-
-    def courses
-        @courses
-    end
-
     def add_course(course)
         @courses << course
     end
 
     def list_all_students(course)
+        course.each do |course|
+            student_names = course.students.mp(&:name)
+            @list_all_students[course.name] = student_names
+        end
+        @list_all_students
+        require 'pry'; binding.pry
     end
 
 end
