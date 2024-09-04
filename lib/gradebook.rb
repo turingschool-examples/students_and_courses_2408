7ae7b3c5d543fb8e71af5273ee669264ec3f9d20
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-09-04 12:00:15 -0500

    feat: added ability to list all students in each course as a hash

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -1,3 +1,27 @@
-class Gradebook
+require 'rspec'
+require './lib/course'
+require './lib/student'
+require 'pry'
 
+
+class Gradebook
+    attr_reader :name,
+                :courses
+
+    def initialize(name)
+        @name = name
+        @courses = []
+    end
+
+    def add_course(course)
+        @courses << course
+    end
+
+    def list_all_students
+        students_hash = {}
+        @courses.each do |course|
+          students_hash[course] = course.students
+        end
+        students_hash
+      end
 end
