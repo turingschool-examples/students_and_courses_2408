f510d4feff3a6179376d36b1cc4c2e8fd21129eb
Michael OBrien <obrien.michael.t@gmail.com>
2024-09-04 12:12:22 -0400

    Provies all the methods for Iteration 1 - Tests from spec file pass

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,29 @@
 class Student
+    attr_reader :name
+                :age
+                :scores
+
+    def initialize(student)
+        # @student_info = student_info
+        @name = student[:name]
+        @age = student[:age]
+        @scores = []
+    end
+
+    def age
+        @age
+    end
+
+    def scores
+        @scores
+    end
+
+    def log_score(score)
+        @scores << score
+    end
+
+    def grade
+        grade = @scores.sum.to_f / 2
+    end
 
 end
