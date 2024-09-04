951ded71897627a42f8d71c86d7623fe2a460ba7
Oscar Patterson <oepatterson12@gmail.com>
2024-09-04 11:38:52 -0400

    spec

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -7,4 +7,30 @@ RSpec.describe Student do
     expect(morgan).to be_instance_of(Student)
   end
 
+  it "has qualities" do 
+    morgan = Student.new({name: "Morgan", age: 21})
+    expect(morgan.name).to eq "Morgan"
+    expect(morgan.age).to eq 21
+    expect(morgan.scores).to eq []
+  end
+
+  it "can log scores" do 
+    morgan = Student.new({name: "Morgan", age: 21})
+    
+    morgan.log_score(89)
+    morgan.log_score(78)
+
+    expect(morgan.scores).to eq [89, 78]
+  end
+
+  it "has grade" do 
+    morgan = Student.new({name: "Morgan", age: 21})
+    
+    morgan.log_score(89)
+    morgan.log_score(78)
+
+    expect(morgan.grade).to eq 83.5
+  end
+
+
 end
