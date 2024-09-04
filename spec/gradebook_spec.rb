c3c4d2ab7941800a56133bde272cd6d92abe0399
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-09-04 11:34:46 -0600

    added required files added list_all_students test

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -1,4 +1,6 @@
 require './lib/gradebook'
+require './lib/course'
+require './lib/student'
 
 RSpec.describe Gradebook do
     it 'exists' do
@@ -24,5 +26,28 @@ RSpec.describe Gradebook do
             expect(gradebook.courses).to include(course1, course2)
             expect(gradebook.courses.size).to eq(2)
         end
-    end            
+    end
+    
+    describe '.list_all_students' do
+        it "will list students in a course" do
+            gradebook = Gradebook.new("Nick Teets")
+            course1 = Course.new("Calculus", 2)
+            course2 = Course.new("Science", 2)
+
+            student1 = Student.new({name: "Morgan", age: 21})
+            student2 = Student.new({name: "Jordan", age: 29})
+            student3 = Student.new({name: "Alice", age: 19})
+
+            course1.enroll(student1)
+            course1.enroll(student2)
+            course2.enroll(student3)
+
+            gradebook.add_course(course1)
+            gradebook.add_course(course2)
+
+            expected_result = { course1 => [student1, student2], course2 => [student3]}
+
+            expect(gradebook.list_all_students).to eq(expected_result)
+        end
+    end
 end
