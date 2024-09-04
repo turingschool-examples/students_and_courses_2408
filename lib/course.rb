ac23abf10ce80eed9f5ddbc5b83a7b85e3e54c28
Oscar Patterson <oepatterson12@gmail.com>
2024-09-04 12:29:04 -0400

    this

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,17 @@
 class Course
+    attr_reader :name, :capacity, :students
+    def initialize(name, capacity)
+        @name = name
+        @capacity = capacity
+        @students = []
+    end
+
+
+    def enroll(student)
+        @students << student
+    end
+
+    def full?
+        
 
 end
