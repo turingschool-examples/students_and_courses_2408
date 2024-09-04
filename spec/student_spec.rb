ff5e48da4d1c2d415a36d6317399385a7da7938b
Natasha Vasquez <nvasquez9020@my.pmi.edu>
2024-09-04 11:00:43 -0500

    codes written with adjacent passing tests

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -7,4 +7,24 @@ RSpec.describe Student do
     expect(morgan).to be_instance_of(Student)
   end
 
+  it 'has an initial score of an empty array' do
+    morgan = Student.new({name: "Morgan", age: 21})
+    expect(morgan.scores).to eq([])
+  end
+
+  it 'can log scores' do
+    morgan = Student.new({name: "Morgan", age: 21})
+    morgan.log_score(89)
+    morgan.log_score(78)
+    expect(morgan.scores).to eq([89, 78])
+  end
+
+  it 'can give a grade average' do
+    morgan = Student.new({name: "Morgan", age: 21})
+    morgan.log_score(89)
+    morgan.log_score(78)
+    expect(morgan.grade).to eq(83.5)
+  end
+
 end
+
