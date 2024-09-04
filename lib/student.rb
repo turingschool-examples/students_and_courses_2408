9d5b8fe5bb1f5183e1aa0f5b49d715a01a5e34fa
Oscar Patterson <oepatterson12@gmail.com>
2024-09-04 11:48:53 -0400

    it1done

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,22 @@
 class Student
+    attr_reader :name,
+                :age,
+                :scores
 
     def initialize(info)
-        
+        @name = info[:name]
+        @age = info[:age]
+        @scores = []
+    end
+
+    def log_score(score)
+        @scores << score
+    end        
+   
+   
+    def grade
+        return 0 if scores.empty?  
+        scores.sum.to_f / scores.size
+
+    end
 end
