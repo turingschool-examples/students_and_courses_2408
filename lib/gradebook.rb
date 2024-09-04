539fdd726ac6bec5477779fafde192c8409b87e0
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-09-04 11:34:00 -0600

    added list_all_students method

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -9,4 +9,12 @@ class Gradebook
     def add_course(course)
         @courses << course
     end
+
+    def list_all_students
+        students_list = {}
+        @courses.each do |course|
+            students_list[course] = course.students
+        end
+        students_list
+    end
 end
