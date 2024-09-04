6e4c6a33d57f1a6660d11cca8fb1dbd6a20b46cd
Seth Verrill <verrill.seth@gmail.com>
2024-09-04 10:43:41 -0600

    feat: Begin Gradebook class

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -1,3 +1,15 @@
 class Gradebook
+  attr_reader :instructor, :courses
+
+  def initialize(instructor)
+    @insructor = instructor
+    @courses = []
+  end
+
+  def add_course(course)
+    @courses << course
+  end
+
+  
 
 end
