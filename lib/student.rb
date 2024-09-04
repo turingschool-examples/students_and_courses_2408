aae64f6ec168963e65953c7f622b89b4c641f398
Danielle Cardona <danielle.cardona23@gmail.com>
2024-09-04 09:31:27 -0600

    initialize student class and add tests for student name and age

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
 class Student
 
+    attr_reader :name,
+                :age
+                
+    def initialize(student_details = {})
+        @name = student_details[:name]
+        @age = student_details[:age]
+    end
+
 end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,9 +2,22 @@ require 'rspec'
 require './lib/student'
 
 RSpec.describe Student do
-  it "exists" do
-    morgan = Student.new({name: "Morgan", age: 21})
-    expect(morgan).to be_instance_of(Student)
+
+  before(:each) do
+    @student = Student.new({name: "Morgan", age: 21})
   end
 
+  describe '#initialze' do
+    it "exists" do
+      expect(@student).to be_instance_of(Student)
+    end
+
+    it 'has a name' do
+      expect(@student.name).to eq("Morgan")
+    end
+
+    it 'has an age' do
+      expect(@student.age).to eq(21)
+    end
+  end
 end
