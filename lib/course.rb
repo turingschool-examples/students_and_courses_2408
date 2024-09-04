02ceb98ce4180a7ab071a85965f90cf08b435047
Danielle Cardona <danielle.cardona23@gmail.com>
2024-09-04 09:49:27 -0600

    initialize Course object and add tests for name, capacity, and students

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,17 @@
 class Course
 
+    attr_reader :name,
+                :capacity
+                :students
+
+    attr_writer :students
+
+    def initialize(name, capacity)
+        @name = name
+        @capacity = capacity
+    end
+
+    def students
+        @students = []
+    end
 end
--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -1,2 +1,27 @@
 require './lib/course'
 require './lib/student'
+
+RSpec.describe Course do
+
+    before(:each) do
+        @course = Course.new("Calculus", 2)
+    end
+
+    describe '#initialze' do
+        it "exists" do
+            expect(@course).to be_instance_of(Course)
+        end
+
+        it 'has a name' do
+            expect(@course.name).to eq("Calculus")
+        end
+
+        it 'has a capacity' do
+            expect(@course.capacity).to eq(2)
+        end
+        
+        it 'starts with an empty array of students' do
+            expect(@course.students).to eq([])
+        end
+    end
+end
