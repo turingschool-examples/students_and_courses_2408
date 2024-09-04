66dacb0d48555788e0a0567114b335c1ebcce8ff
Devlin Lynch <devklynch@gmail.com>
2024-09-04 09:37:57 -0600

    feat: add feat for student for log_score and grade

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
 class Student
 
+    attr_reader :name,
+                :age,
+                :scores
+    
+    def initialize(student)
+        @name = student[:name]
+        @age = student[:age]
+        @scores = []
+    end
+
+    def log_score(score)
+        @scores << score
+    end
+
+    def grade
+        (((@scores.sum).to_f/@scores.count)).round(1)
+    end
+
 end
