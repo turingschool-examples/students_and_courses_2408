d090dd56956dde5f2f50a78465f252548414f808
Jonathan Atkins <jonathanjatkins89@gmail.com>
2024-09-04 09:16:16 -0700

    Completing Iteration 1

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,22 @@
 class Student
+    attr_reader :name,
+                :age,
+                :student_scores
+    def initialize(student_attributes)
+        @name = student_attributes[:name]
+        @age = student_attributes[:age]
+        @student_scores = []
+    end
+
+    def log_score(num)
+        @student_scores << num
+    end
+    
+    def grade
+        return 0 if @student_scores.empty?
+        @student_scores.sum.to_f / @student_scores.size
+    end
+
+
 
 end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,9 +2,34 @@ require 'rspec'
 require './lib/student'
 
 RSpec.describe Student do
-  it "exists" do
-    morgan = Student.new({name: "Morgan", age: 21})
-    expect(morgan).to be_instance_of(Student)
+  before(:each) do
+    @morgan = Student.new({name: "Morgan", age: 21})
+  
   end
+  
+  it "exists" do
+    expect(@morgan).to be_instance_of(Student)
+  end
+
+  it 'Has an empty Array for scores and log them' do
+
+    expect(@morgan.student_scores).to eq([])
+
+    @morgan.log_score(89)
+    expect(@morgan.student_scores).to include(89)
+    
+    @morgan.log_score(78)
+    expect(@morgan.student_scores).to include(78)
+  end
+
+  it "Calculate the Average of the Grades"do
+  
+    @morgan.log_score(89)
+    @morgan.log_score(78)
+    expect(@morgan.grade).to eq(83.5)
+  end
+
+  
+
 
 end
