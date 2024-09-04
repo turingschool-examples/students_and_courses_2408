804bf89ed52b4061e6d5040774ec0d2504546e0e
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-09-04 11:08:36 -0600

    added add course test

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -7,4 +7,22 @@ RSpec.describe Gradebook do
     expect(gradebook).to be_instance_of(Gradebook)
     expect(gradebook.instructor).to eq("Nick Teets")
     end
+   
+    describe '.add_course' do
+        it "will add course to courses" do
+            gradebook = Gradebook.new("Nick Teets")
+            course1 = Course.new("Calculus", 2)
+            course2 = Course.new("Science", 2)
+
+            expect(gradebook.courses).to be_empty
+
+            gradebook.add_course(course1)
+            expect(gradebook.courses).to include(course1)
+            expect(gradebook.courses.size).to eq(1)
+
+            gradebook.add_course(course2)
+            expect(gradebook.courses).to include(course1, course2)
+            expect(gradebook.courses.size).to eq(2)
+        end
+    end            
 end
