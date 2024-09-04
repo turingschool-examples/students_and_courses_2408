c6a3b9df10aca1e52ece002fca2132b4eed16db8
Paul Knapp <ppankluap@gmail.com>
2024-09-04 11:38:31 -0500

    feat: add students attribute to the course class

--- lib/course.rb
+++ lib/course.rb
@@ -1,7 +1,8 @@
 class Course
-    attr_reader :name, :capacity
+    attr_reader :name, :capacity, :students
     def initialize(name, capacity)
         @name = name
         @capacity = capacity
+        @students = []
     end
 end
