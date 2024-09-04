37119dce43b9f39aaf701950fda6a149cb3cb1c3
Alora Riley <alora.riley@gmail.com>
2024-09-04 09:51:46 -0600

    Finish iteration 1

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
 class Student
+    attr_reader :name, :age, :scores, :log_score
+    def initialize(background)
+        @name = background[:name]
+        @age = background[:age]
+        @scores = []
+    end
 
+    def log_score(score)
+        scores << score
+    end
+
+    def grade
+        scores.sum(0.0) / scores.size
+    end
 end
