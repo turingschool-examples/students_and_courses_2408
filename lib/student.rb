94c42ffa3710ff16d1478e7c49697a166a279f08
Paul Knapp <ppankluap@gmail.com>
2024-09-04 11:01:04 -0500

    feat: add and test student class instantiation method

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,8 @@
 class Student
-
+    attr_reader :name, :age, :scores
+    def initialize(info)
+        @name = info[:name]
+        @age = info[:age]
+        @scores = []
+    end
 end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -3,35 +3,35 @@ require './lib/student'
 
 RSpec.describe Student do
   before(:each) do 
-    morgan = Student.new({name: "Morgan", age: 21})
+    @morgan = Student.new({name: "Morgan", age: 21})
   end
 
   it "exists" do
-    expect(morgan).to be_instance_of(Student)
+    expect(@morgan).to be_instance_of(Student)
   end
 
   it "has a name" do
-    expect(morgan.name).to eq ("Morgan")
+    expect(@morgan.name).to eq ("Morgan")
   end
 
   it "has an age" do
-    expect(morgan.age).to eq (21)
+    expect(@morgan.age).to eq (21)
   end
 
   it "starts with no scores" do 
-    expect(morgan.scores).to eq ([])
+    expect(@morgan.scores).to eq ([])
   end
 
-  it "can add scores" do 
+  xit "can add scores" do 
     morgan.log_score(89)
-    expect(morgan.scores).to eq ([89])
-    expect(morgan.scores).not_to eq ([])
+    expect(@morgan.scores).to eq ([89])
+    expect(@morgan.scores).not_to eq ([])
     morgan.log_score(78)
     expect(morgan.scores).to eq ([89, 78])
     expect(morgan.scores).not_to eq ([])
   end
 
-  it "can provide an average score" do 
+  xit "can provide an average score" do 
     morgan.log_score(89)
     morgan.log_score(78)
     expect(morgan.grade).to eq (83.5)
