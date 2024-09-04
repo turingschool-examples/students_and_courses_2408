a3f5aad4b88f818e1f82e29ac4dfa4ebabf434b7
Wally Wallace <jrvw001@gmail.com>
2024-09-04 10:02:54 -0600

    Add psuedo tests for course_spec

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -13,5 +13,36 @@ RSpec.describe Course do
 
   before(:each) do
     @course = Course.new("History", 2)
+    @student1 = Student.new({"Wally", 36})
+    @student2 = Student.new({"Dahlia", 15})
+    @student3 = Student.new({"Brinklee", 10})
+  end
+
+  describe '#enroll' do
+    it 'can enroll a student in a course' do
+      expect(true).to eq(false)
+    end
+
+    it 'can enroll multiple students in a course' do
+      expect(true).to eq(false)
+    end
+  end
+      
+  describe '#full?' do
+    it 'returns false when the course is empty' do
+      expect(true).to eq(false)
+    end
+
+    it 'returns false when the course has students but not to capacity' do
+      expect(true).to eq(false)
+    end
+
+    it 'returns true when the course is at capacity' do
+      expect(true).to eq(false)
+    end
+
+    it 'returns true when the course is over capacity' do
+      expect(true).to eq(false)
+    end
   end
 end
