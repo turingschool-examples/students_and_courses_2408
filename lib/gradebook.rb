839c75c2e429f4d4a562987a795c14ebeb31af0e
Alora Riley <alora.riley@gmail.com>
2024-09-04 11:43:01 -0600

    Add add course and list all students methods

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -1,8 +1,20 @@
 class Gradebook
-
+    attr_reader :instructor, :courses, :course, :students
     def initialize(instructor, courses)
         @instructor = instructor
-        @course = courses[:course]
+        @courses = []
     end
 
+    def add_course(course)
+        courses << course
+        return courses
+    end
+
+    def list_all_students
+        result = {}
+        @courses.each do |course|
+            result[course.name.to_sym] = course.students
+        end
+        result
+    end
 end
