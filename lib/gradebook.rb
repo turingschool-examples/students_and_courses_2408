4220235f9bbed8b73befbfbd3228ead28cc8ac8b
Jonathan Atkins <jonathanjatkins89@gmail.com>
2024-09-04 10:37:59 -0700

    Updating Gradebook Lib

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -1,3 +1,47 @@
+require './lib/course'
+require './lib/student'
+
 class Gradebook
+
+attr_reader :instructor,
+            :courses
+
+    def initialize(instructor)
+        @instructor =instructor
+        @courses = []
+    end
+
+    def add_course(course)
+        @courses << course
+    end
+
+    def list_all_students
+        student_list = {}
+        @courses.each do |course|
+            student_list[course] = course.students
+        end
+        student_list
+    end
+
+    def student_below(threshold)
+        all_students = []
+        @courses.each do |course| 
+            students_in_course = course.students
+            students_below_threshold = []
+            students_in_course.each do |student|
+                if student.grade < threshold
+                    students_below_threshold << student
+                end
+            end
+        all_students += students_below_threshold
+        end
+        all_students
+    end
+        
+
+
+        
+
+
 
 end
