fe8308b471d1e7a9e54ca2803795e0b04432dc82
Michael OBrien <obrien.michael.t@gmail.com>
2024-09-04 14:12:39 -0400

    Creates the gradebook file and methods

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -1,3 +1,27 @@
 class Gradebook
+    attr_reader :instructor
+                :course
+                :list_all_students
+
+    def initialize(instructor)
+        @instructor = instructor
+        @courses = []
+        @list_all_students = {}
+    end
+
+    def instructor
+        @instructor
+    end
+
+    def courses
+        @courses
+    end
+
+    def add_course(course)
+        @courses << course
+    end
+
+    def list_all_students(course)
+    end
 
 end
