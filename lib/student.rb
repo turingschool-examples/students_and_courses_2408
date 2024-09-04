19d0dfbc9bcd6359f6eeae5a6c7949adb9099ef2
Wally Wallace <jrvw001@gmail.com>
2024-09-04 09:47:33 -0600

    Create tests and implementation for Student class

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,20 @@
 class Student
+  attr_reader :student_data, :scores
 
+  def initialize(student_data)
+    @student_data = student_data
+    @scores = []
+  end
+
+  def log_score(score)
+    @scores << score
+  end
+
+  def grade
+    if @scores.length > 0
+      (@scores.sum.to_f / @scores.length.to_f)
+    else
+      nil
+    end
+  end
 end
