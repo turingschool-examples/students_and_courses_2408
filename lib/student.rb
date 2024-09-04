24b1fd0ac48f7cf2488b756d331e657da3be17d1
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-09-04 10:44:36 -0500

    feat: created initialize method

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,10 @@
 class Student
-
+    attr_reader :name,
+                :age,
+                :scores
+    def initialize(student_details)
+        @name = student_details[:name]
+        @age = student_details[:age]
+        @scores = []
+    end
 end
