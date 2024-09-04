0b749e1c48b296c567e89df003a6a2e41bca2874
Devlin Lynch <devklynch@gmail.com>
2024-09-04 09:50:59 -0600

    feat: add initialize for class

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,11 @@
 class Course
+    attr_reader :name,
+                :capacity,
+                :students
 
+    def initialize(name, capacity)
+        @name = name
+        @capacity = capacity
+        @students = []
+    end
 end
