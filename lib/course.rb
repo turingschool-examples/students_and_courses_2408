583e06335fd4837bbfca19473b6bbfec8fc6d5b4
Michael OBrien <obrien.michael.t@gmail.com>
2024-09-04 13:02:10 -0400

    Adds all of the methods for the course_spec.rb file - Tests pass

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,32 @@
 class Course
+    attr_reader :name
+                :capacity
+                :students
+
+    def initialize(name, capacity)
+        @name = name
+        @capacity = capacity
+        @students = []
+    end
+
+    def capacity
+        @capacity
+    end
+
+    def students
+        @students
+    end
+
+    def full?
+        if @capacity == @students.count
+            return true
+        else
+            return false
+        end
+    end
+    
+    def enroll(student)
+        @students << student
+    end
 
 end
