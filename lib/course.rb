251b9a7084a0617a1c86428dc1354e4bb5881011
Wally Wallace <jrvw001@gmail.com>
2024-09-04 10:13:24 -0600

    Add initialize method to Course

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,9 @@
 class Course
-
+  attr_reader :name, :capacity, :students
+  
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @students = []
+  end
 end
--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -1,14 +1,15 @@
 require 'rspec'
 require './lib/course'
+require './lib/student'
 
 RSpec.describe Course do
 
   it 'exists' do
-    expect(true).to eq(false)
     course = Course.new("Calculus",2)
     expect(course).to be_instance_of(Course)
     expect(course.name).to eq("Calculus")
     expect(course.capacity).to eq(2)
+    expect(course.students).to eq([])
   end
 
   before(:each) do
