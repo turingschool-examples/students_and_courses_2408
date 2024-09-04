847cd7a478ad2cc7a5f67e970b1263e3f7163db9
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-09-04 10:02:00 -0600

    fixed some typos and tested grade method

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -10,9 +10,27 @@ RSpec.describe Student do
   describe '.log_scores' do
     it "logs students scores" do
     morgan = Student.new({name: "Morgan", age: 21})
-
+   
     morgan.log_score(89)
+    expect(morgan.scores).to eq([89])
+
     morgan.log_score(78)
+    expect(morgan.scores).to eq([89, 78])
+    end
   end
-end
+
+  describe '.grade' do
+    it 'finds the average of scores' do
+      morgan = Student.new({name: "Morgan", age: 21})
+      
+      morgan.log_score(89)
+      expect(morgan.scores).to eq([89])
+     
+      morgan.log_score(78)
+      expect(morgan.scores).to eq([89, 78])
+
+      morgan.grade
+      expect(morgan.grade).to eq(83.5)
+    end
+  end
 end
