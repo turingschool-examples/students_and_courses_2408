088ee202da64f81db1644c9ae30eb06fdd7a9420
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-09-04 10:40:34 -0600

    added test to see if course.full? is true

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -28,4 +28,16 @@ RSpec.describe Course do
         expect(course.students).to include(student1)
         end
     end
+
+    describe 'enroll' do
+        it 'will say if course if full' do
+        course = Course.new("Calculus", 2)
+        student1 = Student.new({name: "Morgan", age: 21})
+        student2 = Student.new({name: "Jordan", age: 29})
+
+        course.enroll(student1)
+        course.enroll(student2)
+        expect(course.full?).to eq (true)
+        end
+    end
 end
