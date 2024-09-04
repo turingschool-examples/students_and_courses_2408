cf6ab94ab3ebbc8be423aa88f244cc16fa554714
BrodyNics77 <brodynichols777@gmail.com>
2024-09-04 12:55:14 -0500

    finished

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,26 @@
 class Course
 
+    attr_reader :name, :capacity, :students, :full
+
+    def initialize(attrs)
+        @name = attrs[:name]
+        @capacity = attrs[:capacity]
+        @students = []
+        @full = false
+    end
+
+    def enroll(student)
+        @student << student
+    end
+
+    def full?
+        @student.size >= @capacity
+        if 
+            @capacity <= 2
+            then
+            false
+        elsif 
+            true
+        end
+    end
 end
--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -1,3 +1,16 @@
 class Gradebook
 
+    attr_reader :instructor, :coruses, :name
+
+    def initialize(attrs)
+        @instructor = instructor
+        @courses = []
+        @name = name[]
+    end
+
+    def add_courses
+        @courses += 1
+    end
+
+
 end
