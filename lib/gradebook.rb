a5a754ce93dd8073135e290d4b219b7f050a685c
Michael OBrien <obrien.michael.t@gmail.com>
2024-09-04 18:14:59 -0400

    Adds the methods to finish itteration 3 - TESTS PASS

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -13,13 +13,26 @@ class Gradebook
         @courses << course
     end
 
-    def list_all_students(course)
-        course.each do |course|
-            student_names = course.students.mp(&:name)
-            @list_all_students[course.name] = student_names
+    # def list_all_students
+    #     student_hash = {}
+    #     @courses.each do |course|
+    #         student_hash[course] = course.students
+    #         # Alternate way to write the line above to create the hash
+    #         # student_hash.store(course, course.students)
+    #     end
+    #      student_hash
+    # end
+
+    def list_all_students #ALTERNATIVE SOLUTION
+        @courses.reduce({}) do |student_hash, course|
+            student_hash[course] = course.students
+            student_hash
         end
-        @list_all_students
-        require 'pry'; binding.pry
     end
 
+    def students_below(grade_average)
+        @courses.map do |course|
+            course.students.find_all { |student| student.grade <= grade_average}
+        end.flatten.uniq
+    end
 end
