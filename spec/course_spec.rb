c3f31e25d446228afbbc6d4b33b41821ac17341a
Alora Riley <alora.riley@gmail.com>
2024-09-04 10:12:32 -0600

    Pass all tests for iteration 2

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -3,7 +3,9 @@ require './lib/student'
 
 RSpec.describe Course do
     before(:each) do
-        @course = Course.new("Calculus", 2)  
+        @course = Course.new("Calculus", 2) 
+        @student1 = Student.new({name: "Morgan", age: 21}) 
+        @student2 = Student.new({name: "Jordan", age: 29})  
     end
 
     it "exists" do
@@ -14,4 +16,25 @@ RSpec.describe Course do
         expect(@course.name).to eq("Calculus")
         expect(@course.capacity). to eq(2)
     end
+
+    it "students start empty" do
+        expect(@course.students). to eq([])
+    end
+
+    it "can reach capacity" do
+        expect(@course.full?).to eq(false)
+    end
+
+    it "can enroll students" do
+        @course.enroll(@student1)
+        
+        expect(@course.full?).to eq(false)
+        
+        @course.enroll(@student2)
+
+        expect(@course.students).to eq([@student1, @student2])
+        expect(@course.full?).to eq(true)
+    end
+
+
 end
