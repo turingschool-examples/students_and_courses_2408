5085c634c4f4c8904985f453e8a005d8bc319286
Wally Wallace <jrvw001@gmail.com>
2024-09-04 09:56:00 -0600

    Add initialize test to course_spec.rb

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -5,5 +5,13 @@ RSpec.describe Course do
 
   it 'exists' do
     expect(true).to eq(false)
+    course = Course.new("Calculus",2)
+    expect(course).to be_instance_of(Course)
+    expect(course.name).to eq("Calculus")
+    expect(course.capacity).to eq(2)
+  end
+
+  before(:each) do
+    @course = Course.new("History", 2)
   end
 end
