5d9bd3ed7b7773fd6a1f62e95a4be67cb8a233b3
Wally Wallace <jrvw001@gmail.com>
2024-09-04 09:27:52 -0600

    Add psuedo tests to student_spec

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -7,4 +7,25 @@ RSpec.describe Student do
     expect(morgan).to be_instance_of(Student)
   end
 
+  describe '#log_score' do
+    it 'can add a score' do
+      expect(true).to eq(false)
+    end
+
+    it 'can add multiple scores' do
+      expect(true).to eq(false)
+    end
+  end
+
+  describe '#grade' do
+    it 'can return a grade with no scores' do
+      expect(true).to eq(false)
+    end
+    it 'can return a grade with a score' do
+      expect(true).to eq(false)
+    end
+    it 'can return a grade with multiple scores to two decimal places' do
+      expect(true).to eq(false)
+    end
+  end
 end
