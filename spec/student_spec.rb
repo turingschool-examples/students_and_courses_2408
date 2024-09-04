4c4d0b067aff012a5ac62fe70e76c42dd2997d58
Paul Knapp <ppankluap@gmail.com>
2024-09-04 10:31:49 -0500

    test: add a test for the name attribute

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,9 +2,19 @@ require 'rspec'
 require './lib/student'
 
 RSpec.describe Student do
-  it "exists" do
+  before(:each) do 
     morgan = Student.new({name: "Morgan", age: 21})
+  end
+
+  it "exists" do
     expect(morgan).to be_instance_of(Student)
   end
 
+  it "has a name" do
+    expect(morgan.name).to eq ("Morgan")
+  end
+
+  it "has an age" do
+
+  end
 end
