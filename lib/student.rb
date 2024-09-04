6b790fc00acda0e4877f45a1879887651c8ea7bb
Danielle Cardona <danielle.cardona23@gmail.com>
2024-09-04 09:40:18 -0600

    add methods and tests to log scores and calculate grade

--- lib/student.rb
+++ lib/student.rb
@@ -2,10 +2,24 @@ class Student
 
     attr_reader :name,
                 :age
-                
+                :scores 
+
     def initialize(student_details = {})
         @name = student_details[:name]
         @age = student_details[:age]
+        @scores = []
+    end
+
+    def scores
+        @scores
+    end
+
+    def log_score(score)
+        @scores << score
+    end
+
+    def grade
+        (@scores.sum.to_f / @scores.length).round(2)
     end
 
 end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -20,4 +20,32 @@ RSpec.describe Student do
       expect(@student.age).to eq(21)
     end
   end
+
+  describe '#scores' do
+
+  it 'starts with an empty array of scores' do
+    expect(@student.scores).to eq([])
+  end
+
+    it 'can add a score' do
+      @student.log_score(89)
+
+      expect(@student.scores).to eq([89])
+    end
+
+    it 'can add multiple scores' do
+      @student.log_score(89)
+      @student.log_score(78)
+
+      expect(@student.scores).to eq([89, 78])
+    end
+
+    it 'averages all scores to determine a students grade' do
+      @student.log_score(89)
+      @student.log_score(78)
+
+      expect(@student.grade).to eq(83.5)
+    end
+
+  end
 end
