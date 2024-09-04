05fa278b5ecb75f6531dc40ddccf061cda3cdc9c
Oscar Patterson <oepatterson12@gmail.com>
2024-09-04 14:55:12 -0400

    spec

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -13,4 +13,27 @@ RSpec.describe Gradebook do
     it 'can have courses' do
         gradebook1 = Gradebook.new('mr google')
         course1 = Course.new("Calculus", 2)  
-        add_course(course1)
+        gradebook1.add_course(course1)
+
+        expect(gradebook1.courses).to eq [course1]
+    end
+    
+    it 'can list students in course' do
+        gradebook1 = Gradebook.new('mr google')
+        course1 = Course.new("Calculus", 2)  
+        course2 = Course.new("Reading", 3)  
+       
+        student1 = Student.new({name: "Morgan", age: 21})
+        student2 = Student.new({name: "Jordan", age: 29})  
+
+        gradebook1.add_course(course1)
+        gradebook1.add_course(course2)
+        course1.enroll(student1)
+        course1.enroll(student2)
+        course2.enroll(student1)
+
+        expect(gradebook1.courses).to eq [course1, course2]
+        expect(course1.students).to eq [student1, student2]
+    end
+
+end
