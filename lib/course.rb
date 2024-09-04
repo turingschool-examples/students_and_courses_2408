122cbc8057f2419adc22a58e41f7720e9a1486d5
Alora Riley <alora.riley@gmail.com>
2024-09-04 10:11:52 -0600

    Finish iteration 2

--- lib/course.rb
+++ lib/course.rb
@@ -1,8 +1,20 @@
 class Course
-    attr_reader :name, :capacity
+    attr_reader :name, :capacity, :students
     def initialize(name, capacity)
         @name = name
         @capacity = capacity
+        @students = []
     end
 
+    def full?
+        if students.length >= 2
+            true
+        else
+            false
+        end
+    end
+
+    def enroll(student)
+        students << student
+    end
 end
