e1348d281021a21b52d1b61c30fc15c9471bc54f
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-09-04 09:41:17 -0600

    added log_score and grade

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,20 @@
 class Student
 
+    attr_reader :name, :age, :scores
     def initialize(student)
-        name = student[:name]
-        age = student[:age]
+        @name = student[:name]
+        @age = student[:age]
+        @scores = []
+    end
+
+    def log_score(score)
+        @scores << score
+    end
+
+    def grade
+        if @scores.empty? = 0
+            @scores.sum / @scores.size.to_f
+        end
     end
 
 end
