32c1aa34594e2e04fd4cca607b4a5903a87648db
Seth Verrill <verrill.seth@gmail.com>
2024-09-04 15:37:54 -0600

    feat: Add notes for rewrite and min/max feature

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -18,22 +18,46 @@ class Gradebook
     cohort
   end
 
+  # method from video:
+  # def all_grades
+  #   student_hash = {}
+  #   @courses.each do |course|
+  #     grade_array = []
+  #     course.students.each do |student|
+  #       grade_array << student.grade
+  #     end
+  #   end
+  # end
+
   def all_grades
     grades = {}
     @courses.each do |course|
-      grades[course] = course.students.map(&:scores).flatten      
-    end
+      grades[course] = course.students.map(&:scores).flatten    
+    end    
     grades
   end
 
   def students_below_threshold(threshold)
     #rewrite to be more readable
+    #result = @courses.map do |course|
+    #   course.students.find_all { |student| student.grade < threshold }
+    #end.flatten.uniq
+    #flat_map (!!!)
     student_arr = @courses.map(&:students)
     all_students = student_arr.flatten 
     students_not_passing = all_students.select do |student|
       student.scores.any? && student.grade < threshold
     end
     students_not_passing
+  end
+
+  def students_in_range(min, max)
+    @courses.flat_map do |course|
+      course.students.find_all do |student|
+        student.grade >= min && student.grade < max
+      end
+    end.uniq
+  
   end
 
 end
